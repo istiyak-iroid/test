c96bb0d1b7235d584d25837d29beb424316de061
Istiyak <istiyak.iroid@gmail.com>
2019-05-20 10:34:07 +0530

    Istiyak: 20-05-2019

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const express = require("express");
+const logger = require("morgan");
 
 const app = express();
+
+app.use(logger("dev"));
+
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
